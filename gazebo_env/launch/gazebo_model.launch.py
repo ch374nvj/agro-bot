23461fb0a162184a9dedd2c986e5b5e39b9458c6
Chetan V <98468274+ch374nvj@users.noreply.github.com>
2024-12-06 20:37:48 +0530

    Launch File Commit

--- gazebo_env/launch/gazebo_model.launch.py
+++ gazebo_env/launch/gazebo_model.launch.py
@@ -6,3 +6,22 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 import xacro
 
+#the name that is mentioned in the robot.xacro file ref:line6
+robot_xacro_name = 'differential_drive_robot' 
+
+#the package/folder name which the launch file resides
+pkg_name = 'gazebo_env'
+
+#relative paths
+xacro_rel_path = 'model/robot.xacro'
+
+world_rel_path = 'model/empty_world.world'
+
+#Absolute paths
+xacro_path = os.path.join(get_package_share_directory(pkg_name), xacro_rel_path)
+
+world_path = os.path.join(get_package_share_directory(pkg_name), world_rel_path)
+
+#Get robot description from xacro file
+robot_description = xacro.process_file(xacro_path).toxml()
+
