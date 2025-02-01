bbbf595091dfc0e0552fb52e72b2467ee789c65d
Chetan VJ <98468274+ch374nvj@users.noreply.github.com>
2025-02-01 15:06:34 +0530

    Launch File; Minute bug fix

--- gazebo_env/launch/gazebo_model.launch.py
+++ gazebo_env/launch/gazebo_model.launch.py
@@ -48,7 +48,7 @@ def generate_launch_description():
         namespace='',
         executable='rviz2',
         name='rviz',
-        arguments=[' -d' + os.path.join(get_package_share_directory(pkg_name), 'config', 'config.rviz')],
+        arguments=['-d' + os.path.join(get_package_share_directory(pkg_name), 'config', 'config.rviz')],
         parameters=[{'use_sim_time':True}],
     )
 
